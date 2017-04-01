6d83bfaf667ecbb6590dd81560d3366bc2527843
dpgc11 <iamyogeshtelang@gmail.com>
2017-04-01 15:45:01 +0530

    finished initial draft

--- app/src/main/java/com/example/android/mdb/activity/MainActivity.java
+++ app/src/main/java/com/example/android/mdb/activity/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.android.mdb.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import com.example.android.mdb.R;
+import com.example.android.mdb.adapter.MoviesAdapter;
 import com.example.android.mdb.model.Movie;
 import com.example.android.mdb.model.MovieResponse;
 import com.example.android.mdb.rest.ApiClient;
@@ -26,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.movies_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
         ApiInterface apiService =
                 ApiClient.getClient().create(ApiInterface.class);
 
@@ -33,8 +39,9 @@ public class MainActivity extends AppCompatActivity {
         call.enqueue(new Callback<MovieResponse>() {
             @Override
             public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
+                int statusCode = response.code();
                 List<Movie> movies = response.body().getResults();
-                Log.d(TAG, "Number of movies received: " + movies.size());
+                recyclerView.setAdapter(new MoviesAdapter(movies, R.layout.list_item_movie, getApplicationContext()));
             }
 
             @Override
